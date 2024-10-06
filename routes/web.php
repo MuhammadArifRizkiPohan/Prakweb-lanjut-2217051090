488b2b03ecb98b9906740edd99391bba885b93ea
Arif <112569342+MuhammadArifRizkiPohan@users.noreply.github.com>
2024-10-06 14:22:11 +0700

    Selesaikan konflik di routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController; // Tambahkan ini
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Pastikan Anda sudah menghapus atau mengomentari baris berikut jika tidak lagi menggunakan ProfileController
-// use App\Http\Controllers\ProfileController;
 
 // Rute untuk menampilkan profil pengguna
 Route::get('/user/profile/{nama?}/{kelas?}/{npm?}', [UserController::class, 'profile'])->name('user.profile');
 
 // Rute untuk membuat pengguna baru
 Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('/user/store', [UserController::class, 
-'store'])->name('user.store'); 
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
