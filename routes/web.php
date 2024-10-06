9a5b29976769538a2f4e1dc08dd21a09e43aaa5d
Arif <112569342+MuhammadArifRizkiPohan@users.noreply.github.com>
2024-10-06 14:17:04 +0700

    Simpan perubahan  di routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,19 @@
 <?php
-use App\Http\Controllers\ProfileController;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // Tambahkan ini
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+// Pastikan Anda sudah menghapus atau mengomentari baris berikut jika tidak lagi menggunakan ProfileController
+// use App\Http\Controllers\ProfileController;
+
+// Rute untuk menampilkan profil pengguna
+Route::get('/user/profile/{nama?}/{kelas?}/{npm?}', [UserController::class, 'profile'])->name('user.profile');
+
+// Rute untuk membuat pengguna baru
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/user/store', [UserController::class, 
+'store'])->name('user.store'); 
